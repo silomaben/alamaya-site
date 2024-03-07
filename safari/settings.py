e558fc49c1602ece4b98bd5a9bd26217b8629eaf
BenardSilomaMasikond <BenardSilomaMasikond@JITU-PC110VYA>
2024-03-07 23:26:45 +0300

    booking form reforms

--- safari/settings.py
+++ safari/settings.py
@@ -166,7 +166,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'mail.alamayaadventures.com'
 EMAIL_PORT = '25'
 EMAIL_HOST_USER = 'info@alamayaadventures.com'
-EMAIL_HOST_PASSWORD = 'Adrielngugi@gmail.com'
+EMAIL_HOST_PASSWORD = 'LQHrz?7rzqf$'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
