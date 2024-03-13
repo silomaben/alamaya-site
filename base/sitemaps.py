6d6fa7cd1e850a65849c782007e73cdc9b048b1c
BenardSilomaMasikond <BenardSilomaMasikond@JITU-PC110VYA>
2024-03-13 22:34:01 +0300

    changed about us url

--- base/sitemaps.py
+++ base/sitemaps.py
@@ -5,7 +5,7 @@ from .models import Post
 class StaticViewSitemap(Sitemap):
 
     def items(self):
-        return ['home','blog','view_destinations','view_destinations','booking','about-Alamaya-Adventure-Limited','contact-us','gallery','blog','package_1_maasaimara','package_2_hidden_gems','package_3_magical_kenya']
+        return ['home','blog','view_destinations','view_destinations','booking','about-us','contact-us','gallery','blog','package_1_maasaimara','package_2_hidden_gems','package_3_magical_kenya']
     
     def location(self,item):
         return reverse(item)
